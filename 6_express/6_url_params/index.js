a9b5330244b83d354ebcdcd5e00fd75936e782cd
Nathan Lima <nathanls.dev@gmail.com>
2024-07-25 22:07:23 -0300

    realizando buscas com parametros passados na url

---
+++ 6_express/6_url_params/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const door = 3000;
+
+const path = require("path");
+
+const basePath = path.join(__dirname, "templates");
+
+const checkAuth = (req, res, next) => {
+  req.authStatus = true;
+
+  if (req.authStatus) {
+    console.log("User's logged in, continue...");
+    next();
+  } else {
+    console.log("User's not logged, no access!");
+    next();
+  }
+};
+
+app.use(checkAuth);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  console.log(`Searching user: ${id}`)
+
+  res.sendFile(`${basePath}/users.html`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(door, () => {
+  console.log(`App running at door ${door}`);
+});
